64ba835116dc94eae0a28aa427e6923dd1d2df27
Tamas Szeiler <43131215+szeilertamas@users.noreply.github.com>
2024-02-03 08:42:11 +0100

    Secure new Firebase API key with environment variables - Added .env file to store sensitive configuration and new API key - Updated .gitignore to include .env file - Modified Firebase configuration to use environment variables - Deleted the old database to enhance security

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,19 +1,18 @@
 // src/services/firebaseConfig.js
 
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmEn_P6TiX7mckBpXuOHhFM5ptC4znEqg",
-  authDomain: "react-mobx-firebase-app-9cff0.firebaseapp.com",
-  projectId: "react-mobx-firebase-app-9cff0",
-  storageBucket: "react-mobx-firebase-app-9cff0.appspot.com",
-  messagingSenderId: "263326959373",
-  appId: "1:263326959373:web:925acc1d5aa1ae42ad1244"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
-
 const db = getFirestore(firebaseApp);
 
 export { firebaseApp, db };
